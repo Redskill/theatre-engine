9118a8f335900b63580c5f1baf7262749808cec9
Fabio Arosio <fabio_arosio@hotmail.it>
2019-08-19 18:32:23 +0100

    add: crud methods in customer service

--- src/main/kotlin/com/fabio/theatre/theatreengine/database/repository/Repositories.kt
+++ src/main/kotlin/com/fabio/theatre/theatreengine/database/repository/Repositories.kt
@@ -1,16 +1,13 @@
 package com.fabio.theatre.theatreengine.database.repository
 
 //import com.fabio.theatre.theatreengine.database.entity.Booking
-import com.fabio.theatre.theatreengine.database.entity.Booking
-import com.fabio.theatre.theatreengine.database.entity.Customer
-import com.fabio.theatre.theatreengine.database.entity.Review
+import com.fabio.theatre.theatreengine.database.entity.*
 //import com.fabio.theatre.theatreengine.database.entity.Review
-import com.fabio.theatre.theatreengine.database.entity.Seat
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface BookingRepository : JpaRepository<Booking, Int>
-interface ReviewRepository : JpaRepository<Review, Int>
 interface CustomerRepository : JpaRepository<Customer, Int>
+interface ReviewRepository : JpaRepository<Review, Int>
 interface SeatRepository : JpaRepository<Seat, Int>
-
-
+interface ShowRepository : JpaRepository<Show, Int>
+interface VenueRepository : JpaRepository<Venue, Int>
--- src/main/kotlin/com/fabio/theatre/theatreengine/service/CustomerService.java
+++ src/main/kotlin/com/fabio/theatre/theatreengine/service/CustomerService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -16,9 +17,6 @@ public class CustomerService {
 
     @Autowired
     private CustomerRepository customerRepository;
-
-    @Autowired
-    private BookingRepository bookingRepository;
 
     public List<Customer> getAllCustomers() {
         List<Customer> list = customerRepository.findAll();
@@ -32,24 +30,45 @@ public class CustomerService {
         // return customerRepository.findAll();
     }
 
-    private List<Booking> aMethod(Integer customerId) {
-        List<Booking> bookingList = bookingRepository.findAll();
-        List<Booking> customerBooking = new ArrayList<>();
-
-        bookingList.forEach(booking -> {
-            if (booking.getCustomer().getId() == customerId) {
-                customerBooking.add(booking);
-            }
-        });
-        List<Booking> bookingStram = bookingList.stream()
-                .filter(e -> e.getCustomer().getId() == customerId)
-                .collect(Collectors.toList());
-
+//    private List<Booking> aMethod(Integer customerId) {
+//        List<Booking> bookingList = bookingRepository.findAll();
+//        List<Booking> customerBooking = new ArrayList<>();
+//
+//        bookingList.forEach(booking -> {
+//            if (booking.getCustomer().getId() == customerId) {
+//                customerBooking.add(booking);
+//            }
+//        });
+//        List<Booking> bookingStram = bookingList.stream()
+//                .filter(e -> e.getCustomer().getId() == customerId)
+//                .collect(Collectors.toList());
+//
 //        for (Booking booking : bookingList) {
 //            if (booking.getCustomer().getId() == customerId) {
 //                customerBooking.add(booking);
 //            }
 //        }
-        return customerBooking;
+//        return customerBooking;
+//    }
+
+    public Customer getCustomerById(Integer id) {
+        return customerRepository.findById(id).orElseThrow(RuntimeException::new);
+    }
+
+    public void deleteById(Integer id) {
+        Optional<Customer> customer = customerRepository.findById(id);
+        if (customer.isPresent()) {
+            customerRepository.deleteById(id);
+        }
+    }
+
+    public Customer updateCustomer(Customer customer) {
+        customerRepository.save(customer);
+        return customer;
+    }
+
+    public Customer saveCustomer(Customer customer) {
+        customerRepository.save(customer);
+        return customer;
     }
 }
